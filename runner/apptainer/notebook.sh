5980a8342fb69ba3a39bf4f868969188e13ee8a2
dialecticDolt <will.ruys@gmail.com>
2024-01-03 22:23:26 -0500

    fix: Jupyter password auth (prevent injection vulnerability to compute node just in case)

--- runner/apptainer/notebook.sh
+++ runner/apptainer/notebook.sh
@@ -7,12 +7,13 @@ if [ "$1" == "-h" ]; then
     exit 1
 fi
 
+PASSWORD_HASH='sha1:b65489bbd2d0:7f0c0ba157c5e88132db52a6f5a726dbc9ce4d0a'
+
 # Check if the first argument is --use-gpu
 if [ "$1" == "--use-gpu" ]; then
     echo "Running with GPU support..."
-    docker run -p 8888:8888 --volume $(pwd):/app --workdir /app utaustin/nuwest jupyter notebook --ip="*" --NotebookApp.token='' -y --port=8888 --no-browser 
-
+    apptainer run --cleanenv --nv utaustin_nuwest.sif jupyter notebook --ip="*" --PasswordIdentityProvider.hashed_password=$PASSWORD_HASH -y --port=8888 --no-browser
 else
     echo "Running without GPU support..."
-    docker run -p 8888:8888 --runtime=nvidia --gpus all --volume $(pwd):/app --workdir /app utaustin/nuwest jupyter notebook --ip="*" --NotebookApp.token='' -y --port=8888 --no-browser 
+    apptainer run --cleanenv utaustin_nuwest.sif jupyter notebook --ip="*" --PasswordIdentityProvider.hashed_password=$PASSWORD_HASH -y --port=8888 --no-browser
 fi
