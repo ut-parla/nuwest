4228f699da622442f040cc1662037daaa0dd72f5
jfabell <70980257+jfabell@users.noreply.github.com>
2024-01-09 12:15:16 -0600

    Delete lessons/boltzmanndemo directory

--- lessons/boltzmanndemo/run.sh
+++
@@ -1,4 +0,0 @@
-rm -r pk_cpp
-python main.py -nw -s 200 -g 1200 -e 3.5 -N 1000000 -name test_ -n 1 -p -ng 1
-python main.py -nw -s 200 -g 1200 -e 3.5 -N 2000000 -name test_ -n 1 -p -ng 2
-python main.py -nw -s 200 -g 1200 -e 3.5 -N 3000000 -name test_ -n 1 -p -ng 3
