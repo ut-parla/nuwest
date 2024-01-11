979b7cb138a01650a5c43e875aa12e092ee9f70e
wlruys <will.ruys@gmail.com>
2024-01-11 10:41:57 -0600

    update: fix path

--- runner/apptainer/install.sh
+++ runner/apptainer/install.sh
@@ -2,6 +2,11 @@
 
 DOCKER_IMAGE=wlruys/nuwest:cpu
 
+# Read the image name from the command line
+if [ $# -eq 1 ]; then
+    DOCKER_IMAGE=$1
+fi
+
 # Pull the latest version of the docker image
 apptainer pull utaustin_nuwest.sif docker://$DOCKER_IMAGE
 
