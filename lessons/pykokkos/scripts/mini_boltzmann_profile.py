30e89f3ce8958e81b0f5fd79ac0c6780e33468f2
Milos Gligoric <milos.gligoric@gmail.com>
2024-01-12 14:55:43 -0600

    pykokkos: Add profile block

---
+++ lessons/pykokkos/scripts/mini_boltzmann_profile.py
@@ -0,0 +1,51 @@
+import argparse
+import numpy as np
+from cProfile import Profile
+from pstats import SortKey, Stats
+
+import pykokkos as pk
+pk.set_default_space(pk.OpenMP)
+
+from advection_kernel import advect
+
+def main(in_N, in_steps):
+    threads_per_block = 64
+    num_blocks = 1024
+
+    N = in_N
+    num_steps = in_steps
+
+    # Set up data structures.
+    x_ar = np.random.rand(N)
+    v_ar = 0.01*np.random.rand(N)
+    E_ar = np.zeros(N)
+    R_ar = np.random.rand(N)
+    
+    print("Beginning average position =",np.mean(x_ar))
+
+    for step in range(num_steps):
+
+        # Draw random numbers.
+        R_ar[:] = np.random.rand(N)
+
+        # Generate random electric field.
+        E_ar.fill(0.01 * np.random.rand())
+        
+        # PyKokkos kernel for particle advection + collision.
+        advect(N, x_ar, v_ar, E_ar, R_ar, threads_per_block, num_blocks)
+
+    print("End average position =", np.mean(x_ar))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-N", "--num_particles", type=int, default=100000)
+    parser.add_argument("-s", "--num_steps", type=int, default=100)
+    args = parser.parse_args()
+    with Profile() as profile:
+        main(args.num_particles, args.num_steps)
+        ( Stats(profile).
+          strip_dirs()
+          .sort_stats(SortKey.CUMULATIVE)
+          .print_stats()
+         )
