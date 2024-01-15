6238269d07e2d894d31d33772b62e0dd76404aac
jfabell <70980257+jfabell@users.noreply.github.com>
2024-01-15 16:27:07 -0600

    Update advection_kernel_hp.py

--- lessons/pykokkos/scripts/advection_kernel_hp.py
+++ lessons/pykokkos/scripts/advection_kernel_hp.py
@@ -3,21 +3,21 @@ import pykokkos as pk
 @pk.workunit()
 def advection_kernel(
     team_member,
-    Nc, 
-    d_x_ar, 
-    d_v_ar, 
-    d_E_ar, 
-    d_R_ar,
+    N, 
+    x_ar, 
+    v_ar, 
+    E_ar, 
+    R_ar,
     stride,
 ):
     j: int = team_member.league_rank()
 
     def inner(i: int):
         # Looping and doing all the particles.
-        d_x: float = d_x_ar[i]
-        d_v: float = d_v_ar[i]
-        d_E: float = d_E_ar[i]
-        d_R: float = d_R_ar[i]
+        d_x: float = x_ar[i]
+        d_v: float = v_ar[i]
+        d_E: float = E_ar[i]
+        d_R: float = R_ar[i]
         
         # Collision - each particle has 10% probability of collision, which flips velocity.
         if (d_R < 0.1):
@@ -38,17 +38,17 @@ def advection_kernel(
             d_v = -d_v       
         
         # Put data back into arrays.
-        d_x_ar[i] = d_x
-        d_v_ar[i] = d_v
-        d_E_ar[i] = d_E
+        x_ar[i] = d_x
+        v_ar[i] = d_v
+        E_ar[i] = d_E
     pk.parallel_for(pk.TeamThreadRange(team_member, stride), inner)
 
 def advect(
-    Nc,
-    d_x_ar,
-    d_v_ar,
-    d_E_ar,
-    d_R_ar,
+    N,
+    x_ar,
+    v_ar,
+    E_ar,
+    R_ar,
     threads_per_block,
     num_blocks,
 ):
@@ -56,10 +56,10 @@ def advect(
     pk.parallel_for(
         pk.TeamPolicy(num_blocks, pk.AUTO),
         advection_kernel,
-        Nc=Nc,
-        d_x_ar=d_x_ar,
-        d_v_ar=d_v_ar,
-        d_E_ar=d_E_ar,
-        d_R_ar=d_R_ar,
+        N=N,
+        x_ar=x_ar,
+        v_ar=v_ar,
+        E_ar=E_ar,
+        R_ar=R_ar,
         stride=threads_per_block,
     )
